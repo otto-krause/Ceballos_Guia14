86911bb666d4f68dd38175194c22c327b67599c0
CeballosLeandro <86359478+CeballosLeandro@users.noreply.github.com>
2021-11-24 10:38:09 -0300

    Add files via upload

--- g14_4.c
+++ g14_4.c
@@ -1,20 +1,39 @@
-#include<stdio.h>
-int main()
-{ int c[3][12], tc=0, tl=0;
-  for(int f=0;f<3;f++){
-  for(int x=0;x<12;x++){
-  printf("Ingrese lo recaudado del coche %i en la linea %i: ",x+1,f+1);
-  scanf("%i", &c[f][x]);}
-  }
-  printf("---------------------------------------------------------------\n");
-  for(int f=0;f<3;f++){
-  for(int x=0;x<12;x++){
-  tc+=c[f][x];
-  tl+=c[f][x];
-  printf("La recaudacion total del coche %i en la linea %i es: %i\n",x+1,f+1,c[f][x]);
-  }
-     printf("La recaudacion total de la linea %i es: %i\n",f+1,tl);
-  }
-  printf("La recaudacion total general es: %i\n", tc+tl);
-  return 0;
+#include <stdio.h>
+#include <stdlib.h>
+void main()
+{ float c[3][12], imp, recaudacionLinea[3], recaudacionCoche[12], recaudacionGeneral = 0;
+  int i, x, linea, continuar, coche;
+  for (i = 0;i<3;i++) {
+    recaudacionLinea[i] = 0;
+    for (x = 0;x<12;x++){
+        c[i][x] = 0;
+        recaudacionCoche[x] = 0;
+        }
+    }
+        for(int i=0;i<3;i++){
+        printf("Ingresar linea ( 1 - 3): "); scanf("%d", &linea);
+        for(int x=0;x<12;x++){
+        printf("Ingresar coche ( 1 - 12): "); scanf("%d", &coche);
+        printf("Ingresar recaudacion: "); scanf("%f", &imp);
+        c[linea-1][coche-1] += imp;
+        }
+      }
+
+    for (i = 0;i<3;i++) {
+        for (x = 0;x<12;x++){
+            recaudacionLinea[i] += c[i][x];
+            recaudacionCoche[x] += c[i][x];
+            recaudacionGeneral += c[i][x];
+        }
+    }
+
+    printf("Recuadacion total: %.2f\n", recaudacionGeneral);
+    printf("Recuadacion Linea 1: %.2f\n", recaudacionLinea[0]);
+    printf("Recuadacion Linea 2: %.2f\n", recaudacionLinea[1]);
+    printf("Recuadacion Linea 3: %.2f\n\n", recaudacionLinea[2]);
+    for (x = 0;x<12;x++){
+        printf("Recuadacion Coche %d: %.2f\n", x+1, recaudacionCoche[x]);
+    }
+
+    system("pause");
 }
