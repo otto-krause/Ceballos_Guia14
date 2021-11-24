1c6d34c1521ff57842a00caead9c3caba1dbfe25
CeballosLeandro <86359478+CeballosLeandro@users.noreply.github.com>
2021-11-24 11:08:39 -0300

    Add files via upload

--- g14_4.c
+++ g14_4.c
@@ -2,37 +2,41 @@
 #include <stdlib.h>
 void main()
 { float c[3][12], imp, rLinea[3], rCoche[12], rGeneral = 0;
-  int i, x, linea, continuar, coche;
-  for (i = 0;i<3;i++) {
+  int l, coc;
+  for (int i = 0;i<3;i++) {
     rLinea[i] = 0;
-    for (x = 0;x<12;x++){
+    for (int x = 0;x<12;x++){
         c[i][x] = 0;
         rCoche[x] = 0;
         }
     }
         for(int i=0;i<3;i++){
-        printf("Ingresar linea ( 1 - 3): "); scanf("%d", &linea);
+        printf("Ingresar linea ( 1 - 3): "); scanf("%d", &l);
+        if(l>1){printf("La linea debe ser entre 1 - 3"); continue;
+        }
         for(int x=0;x<12;x++){
-        printf("Ingresar coche ( 1 - 12): "); scanf("%d", &coche);
+        printf("Ingresar coche ( 1 - 12): "); scanf("%d", &coc);
+        if(c>12){printf("El coche debe estar entre 1 - 12");
+        continue;}
         printf("Ingresar recaudacion: "); scanf("%f", &imp);
-        c[linea-1][coche-1] += imp;
+        c[l-1][coc-1]+=imp;
         }
       }
 
-    for (i = 0;i<3;i++) {
-        for (x = 0;x<12;x++){
-            rLinea[i] += c[i][x];
-            rCoche[x] += c[i][x];
-            rGeneral += c[i][x];
+    for (int i = 0;i<3;i++) {
+        for (int x = 0;x<12;x++){
+            rLinea[i]+=c[i][x];
+            rCoche[x]+=c[i][x];
+            rGeneral+=c[i][x];
         }
     }
     system("cls");
-    printf("Recuadacion total: %.2f\n", rGeneral);
-    printf("Recuadacion Linea 1: %.2f\n", rLinea[0]);
-    printf("Recuadacion Linea 2: %.2f\n", rLinea[1]);
-    printf("Recuadacion Linea 3: %.2f\n\n", rLinea[2]);
-    for (x = 0;x<12;x++){
-     printf("Recuadacion Coche %d: %.2f\n", x+1, rCoche[x]);
+    printf("La Recuadacion total es : %.2f\n", rGeneral);
+    for(int i=0;i<3;i++){
+    printf("Recuadacion Linea %i: %.2f\n",i+1,rLinea[i]);
+    }
+    for (int x=0;x<12;x++){
+     printf("Recuadacion Coche %i: %.2f\n", x+1, rCoche[x]);
     }
 
     system("pause");
